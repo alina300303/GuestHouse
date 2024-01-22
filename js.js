352fa39596a217df5b2351a29313f03c32c0b10a
Alina <alinabegenchevna6@gmail.com>
2024-01-22 18:04:53 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,8 @@
+let burger = document.querySelector('.burger')
+let burgerBtn = document.querySelector('.burger__btn')
+
+
+let toggleBurger = ()=>{
+    burger.classList.toggle('active')
+    burgerBtn.classList.toggle('active')
+}
